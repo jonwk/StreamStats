935dcfc1e41555b7e25bf6d7f55bdd46a939bed6
John Kommala <johnwesleygithub@gmail.com>
2025-02-06 22:37:13 +0000

    added FadeIn Animation to the RecentlyPlayed Page

--- client/src/pages/RecentlyPlayed.js
+++ client/src/pages/RecentlyPlayed.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Loader, SectionWrapper, TrackItem } from 'src/components'
+import FadeIn from 'src/animations/FadeIn'
 import { getRecentlyPlayed } from 'src/spotify'
 import { StyledTrackList } from 'src/styles'
 import { catchErrors } from 'src/util'
@@ -12,24 +13,28 @@ const RecentlyPlayed = () => {
         const fetchData = async () => {
             const { data } = await getRecentlyPlayed()
             setRecentlyPlayed(data)
+            console.log('recentlyPlayed', data)
         }
         catchErrors(fetchData())
     }, [])
 
     return (
-        <main>
-            <SectionWrapper title="Recently Played" breadcrumb={true}>
-                {recentlyPlayed && recentlyPlayed.items ? (
-                    // <RecentlyPlayedList tracks={recentlyPlayed.items} />
-                    <StyledTrackList>
-                        {recentlyPlayed.items.map(({ track }, index) => (<TrackItem track={track} key={index} />))}
-                    </StyledTrackList>
-                ) : (
-                    <Loader />
-                )}
-            </SectionWrapper>
-        </main>
-
+        <FadeIn>
+            <main>
+                <SectionWrapper title="Recently Played" breadcrumb={true}>
+                    {recentlyPlayed && recentlyPlayed.items ? (
+                        // <RecentlyPlayedList tracks={recentlyPlayed.items} />
+                        <FadeIn delay={100}>
+                            <StyledTrackList>
+                                {recentlyPlayed.items.map(({ track }, index) => (<TrackItem track={track} key={index} />))}
+                            </StyledTrackList>
+                        </FadeIn>
+                    ) : (
+                        <Loader />
+                    )}
+                </SectionWrapper>
+            </main>
+        </FadeIn >
     )
 }
 
