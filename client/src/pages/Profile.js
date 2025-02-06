4349af1397afd3b457089684be78344542b20219
John Kommala <johnwesleygithub@gmail.com>
2025-02-06 20:33:07 +0000

    using FadeIn Animation in Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -14,9 +14,9 @@ import {
   getCurrentUserTopTracks,
   getRecentlyPlayed
 } from 'src/spotify'
+import FadeIn from 'src/animations/FadeIn'
 import { StyledHeader, StyledTrackList } from 'src/styles'
 import { catchErrors } from 'src/util'
-
 
 
 const Profile = () => {
@@ -52,61 +52,67 @@ const Profile = () => {
   return (
     <div>
       {profile && (
-        <div>
-          <StyledHeader type="user">
-            <div className="header__inner">
-              {profile.images && profile.images.length > 0 && profile.images[0].url && (
-                <p>
-                  <img
-                    className="header__img"
-                    src={profile.images[0].url}
-                    alt="profile-pic"
-                  />
-                </p>
-              )}
-              <div>
-                <div className="header__overline">Profile</div>
-                <h1 className="header__name">{profile.display_name}</h1>
-                <p className="header__meta">
-                  {playlists && (
+        <FadeIn>
+          <div>
+            <StyledHeader type="user">
+              <div className="header__inner">
+                {profile.images && profile.images.length > 0 && profile.images[0].url && (
+                  <p>
+                    <img
+                      className="header__img"
+                      src={profile.images[0].url}
+                      alt="profile-pic"
+                    />
+                  </p>
+                )}
+                <div>
+                  <div className="header__overline">Profile</div>
+                  <h1 className="header__name">{profile.display_name}</h1>
+                  <p className="header__meta">
+                    {playlists && (
+                      <span>
+                        {playlists.total} Playlist
+                        {playlists.total === 1 ? '' : 's'}
+                      </span>
+                    )}
                     <span>
-                      {playlists.total} Playlist
-                      {playlists.total === 1 ? '' : 's'}
+                      {profile.followers.total} Follower
+                      {profile.followers.total === 1 ? '' : 's'}
                     </span>
-                  )}
-                  <span>
-                    {profile.followers.total} Follower
-                    {profile.followers.total === 1 ? '' : 's'}
-                  </span>
-                </p>
+                  </p>
+                </div>
               </div>
-            </div>
-          </StyledHeader>
+            </StyledHeader>
 
-          <main>
-            <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
-              {topArtists ? (<ArtistsGrid artists={topArtists.items.slice(0, 10)} />) : (<Loader />)}
-            </SectionWrapper>
+            <main>
+              <FadeIn delay={100}>
+                <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
+                  {topArtists ? (<ArtistsGrid artists={topArtists.items.slice(0, 10)} />) : (<Loader />)}
+                </SectionWrapper>
 
-            <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
-              {topTracks ? (<TrackList tracks={topTracks.items.slice(0, 10)} />) : (<Loader />)}
-            </SectionWrapper>
+                <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
+                  {topTracks ? (<TrackList tracks={topTracks.items.slice(0, 10)} />) : (<Loader />)}
+                </SectionWrapper>
 
-            <SectionWrapper title="Recently Played" seeAllLink="/recently-played">
-              {/* {topTracks ? (<TrackList tracks={topTracks.items.slice(0, 10)} />) : (<Loader />)} */}
-              {/* {recentlyPlayed ? (<TrackList tracks={recentlyPlayed.data.items.slice(0, 10)} />) : (<Loader />)} */}
-              {recentlyPlayed ? (
-                <StyledTrackList>
-                  {recentlyPlayed.items.slice(0, 10).map(({ track }, index) => (<TrackItem track={track} key={index} />))}
-                </StyledTrackList>) : (<Loader />)}
-            </SectionWrapper>
+                <SectionWrapper title="Recently Played" seeAllLink="/recently-played">
+                  {recentlyPlayed ? (
+                    <StyledTrackList>
+                      {recentlyPlayed.items.slice(0, 10).map(({ track }, index) => (
+                        <TrackItem track={track} key={index} />
+                      ))}
+                    </StyledTrackList>
+                  ) : (<Loader />)}
+                </SectionWrapper>
 
-            <SectionWrapper title="Public Playlists" seeAllLink="/playlists">
-              {playlists ? (<PlaylistsGrid playlists={playlists.items.slice(0, 10)} />) : (<Loader />)}
-            </SectionWrapper>
-          </main>
-        </div>
-      )}
+                <SectionWrapper title="Public Playlists" seeAllLink="/playlists">
+                  {playlists ? (<PlaylistsGrid playlists={playlists.items.slice(0, 10)} />) : (<Loader />)}
+                </SectionWrapper>
+              </FadeIn>
+            </main>
+          </div>
+        </FadeIn>
+      )
+      }
     </div>
   )
 }
