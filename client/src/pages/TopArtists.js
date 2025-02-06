6667f0db75434e330292a94be385c754cd52b9da
John Kommala <johnwesleygithub@gmail.com>
2025-02-06 21:49:26 +0000

    added fade in animations to TopArtists

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ArtistsGrid, Loader, SectionWrapper, TimeRangeButtons } from 'src/components'
+import FadeIn from 'src/animations/FadeIn'
 import { getCurrentUserTopArtists } from 'src/spotify'
 import { catchErrors } from 'src/util'
 
@@ -7,31 +8,43 @@ import { catchErrors } from 'src/util'
 const TopArtists = () => {
     const [topArtists, setTopArtists] = useState()
     const [activeRange, setActiveRange] = useState('short')
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true)
             const { data } = await getCurrentUserTopArtists(`${activeRange}_term`)
             setTopArtists(data)
+            setIsLoading(false)
         }
 
         catchErrors(fetchData())
     }, [activeRange])
 
     return (
-        <main>
-            <SectionWrapper title="Top Artists" breadcrumb={true}>
-                <TimeRangeButtons
-                    activeRange={activeRange}
-                    setActiveRange={setActiveRange}
-                />
-
-                {topArtists && topArtists.items ? (
-                    <ArtistsGrid artists={topArtists.items} />
-                ) : (
-                    <Loader />
-                )}
-            </SectionWrapper>
-        </main>
+        <FadeIn>
+            <main>
+                <SectionWrapper title="Top Artists" breadcrumb={true}>
+                    <TimeRangeButtons
+                        activeRange={activeRange}
+                        setActiveRange={setActiveRange}
+                    />
+                    <div style={{ minHeight: '60vh', position: 'relative' }}>
+                        {!isLoading && topArtists && topArtists.items ? (
+                            <FadeIn
+                                key={activeRange}
+                                delay={100}
+                                transitionDuration={600}
+                            >
+                                <ArtistsGrid artists={topArtists.items} />
+                            </FadeIn>
+                        ) : (
+                            <Loader />
+                        )}
+                    </div>
+                </SectionWrapper>
+            </main>
+        </FadeIn>
     )
 }
 
