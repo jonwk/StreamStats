0b9da425f0973df7727fb83101fa475220517823
John Kommala <johnwesleygithub@gmail.com>
2025-04-21 15:21:28 +0100

    removed previous next app

--- streamstats-next/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['i.scdn.co', 'mosaic.scdn.co', 'platform-lookaside.fbsbx.com'],
-  },
-};
-
-export default nextConfig;
