d8a1b84e3307c08add367715eaf9af13c7af20f3
John Kommala <johnwesleygithub@gmail.com>
2025-04-23 21:59:51 +0100

    login page

---
+++ my-app/src/app/login/page.js
@@ -0,0 +1,41 @@
+'use client'
+import styled from 'styled-components'
+import StyledComponentsRegistry from '../../lib/registry'
+
+const StyledLoginContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const StyledLoginButton = styled.a`
+  display: inline-block;
+  background-color: var(--green);
+  color: var(--white);
+  border-radius: var(--border-radius-pill);
+  font-weight: 700;
+  font-size: var(--fz-lg);
+  padding: var(--spacing-sm) var(--spacing-xl);
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    filter: brightness(1.1);
+  }
+`
+
+const LOGIN_URI =
+  process.env.NODE_ENV === 'production'
+    ? 'https://spotifics.vercel.app/login'
+    : 'http://localhost:3000/'
+
+
+const Login = () => (
+  <StyledLoginContainer>
+    <StyledLoginButton href={LOGIN_URI}>Log in to Spotify</StyledLoginButton>
+  </StyledLoginContainer>
+)
+
+export default Login
