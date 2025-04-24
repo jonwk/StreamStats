8bcd74eeb0a6213622adcce54b67c8a937903ea8
John Kommala <johnwesleygithub@gmail.com>
2025-04-24 19:50:19 +0100

    added refresh token management for the next auth spotify

--- my-app/src/app/api/auth/[...nextauth]/route.js
+++ my-app/src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,41 @@
 import NextAuth from "next-auth/next";
 import SpotifyProvider from 'next-auth/providers/spotify';
 
+const SPOTIFY_REFRESH_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
+
+async function refreshAccessToken(token) {
+  try {
+    const basicAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+      'base64'
+    )
+    const { data } = await axios.post(
+      SPOTIFY_REFRESH_TOKEN_URL,
+      {
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken,
+      },
+      {
+        headers: {
+          Authorization: `Basic ${basicAuth}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+    return {
+      ...token,
+      accessToken: data.access_token,
+      accessTokenExpires: Date.now() + data.expires_in * 1000,
+    }
+  } catch (error) {
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    }
+  }
+}
+
 const scope = [
   'user-read-private',
   'user-read-email',
@@ -15,22 +50,31 @@ const options = {
       authorization: {
         params: { scope }
       },
-      clientId: process.env.SPOTIFY_CLIENT_ID || '',
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET || '',
+      clientId: CLIENT_ID || '',
+      clientSecret: CLIENT_SECRET || '',
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.access_token = account.access_token;
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          accessTokenExpires: account.expires_at * 1000,
+          user
+        }
       }
-      return token;
+      if (token.accessTokenExpires && Date.now() < token.accessTokenExpires) {
+        return token
+      }
+      const newToken = await refreshAccessToken(token)
+      return newToken
     },
     async session({ session, token }) {
-      return {
-        ...session,
-        token
-      };
+      session.accessToken = token.accessToken
+      session.error = token.error
+      session.user = token.user
+      return session
     },
   }
 }
