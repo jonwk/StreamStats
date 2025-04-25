499c80adfd132d689f0659242e3147f1d8aa1121
John Kommala <johnwesleygithub@gmail.com>
2025-04-25 21:03:12 +0100

    updated index exports components

--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -1,8 +1,9 @@
 export { default as ArtistsGrid } from '~/components/ArtistsGrid'
 export { default as Loader } from '~/components/Loader'
+export {default as LogoutButton} from '~/components/LogoutButton'
 export { default as PlaylistsGrid } from '~/components/PlaylistsGrid'
+export {default as ScrollToTop} from '~/components/ScrollToTop'
 export { default as SectionWrapper } from '~/components/SectionWrapper'
 export { default as TimeRangeButtons } from '~/components/TimeRangeButtons'
 export { default as TrackItem } from '~/components/TrackItem'
 export { default as TrackList } from '~/components/TrackList'
-// export { default as FeatureChart } from './FeatureChart';
