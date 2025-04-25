c07760a4e332aeae611d9b987de5e39bebdeeaaf
John Kommala <johnwesleygithub@gmail.com>
2025-04-25 20:02:58 +0100

    updated getwrapper

--- my-app/src/app/api/spotify/getWrapper.js
+++ my-app/src/app/api/spotify/getWrapper.js
@@ -1,9 +1,9 @@
 import { getSession } from "next-auth/react";
 
-const getWrapper = async (urlEndpoint) => {
+const getWrapper = async ({urlEndpoint, url}) => {
   const { accessToken } = await getSession()
   const baseUrl = 'https://api.spotify.com/v1'
-  const res = await fetch(`${baseUrl}${urlEndpoint}`, {
+  const res = await fetch(url ? url :`${baseUrl}${urlEndpoint}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
