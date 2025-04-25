ae680c3eb67d3880998d0102b3c5df079fd32d5a
John Kommala <johnwesleygithub@gmail.com>
2025-04-25 21:03:37 +0100

    auth layout with a logout button

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,23 +1,35 @@
 'use client';
 
-import { Inter } from "next/font/google";
 import AuthProvider from "~/app/AuthProvider";
 import StyledComponentsRegistry from '~/lib/registry'
 import theme from '~/styles/theme'
 import GlobalStyles from '~/styles/GlobalStyles'
 import { ThemeProvider } from 'styled-components'
+import { useSession } from "next-auth/react"
+import { LogoutButton, ScrollToTop } from "~/components"
 
-const inter = Inter({ subsets: ["latin"] });
+const AuthenticatedLayout = ({ children }) => {
+  const { data: session } = useSession()
+  return (
+    <>
+      {session && <LogoutButton />}
+      <ScrollToTop />
+      {children}
+    </>
+  )
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className='streamstas-body'>
         <AuthProvider>
           <StyledComponentsRegistry>
             <GlobalStyles />
             <ThemeProvider theme={theme}>
-              {children}
+              <AuthenticatedLayout>
+                {children}
+              </AuthenticatedLayout>
             </ThemeProvider>
           </StyledComponentsRegistry>
         </AuthProvider>
