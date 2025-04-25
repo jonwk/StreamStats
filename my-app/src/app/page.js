3c8db2eff2266e4ada74762981159a03fdef9b93
John Kommala <johnwesleygithub@gmail.com>
2025-04-25 18:39:40 +0100

    updated base page and removed login route

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,39 +1,51 @@
-
 'use client'
+import styled from 'styled-components'
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import { signIn, signOut } from "next-auth/react";
+import Link from 'next/link';
 
-export default function Home() {
-  const { data: session } = useSession();
+const StyledLoginContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
 
-  if (session) {
-    return (
-      <div className='max-w-[19rem] h-[22rem] rounded-[2rem] border-4 border-solid border-white flex justify-around items-center flex-col flex-nowrap mt-10 ml-10 mb-16'>
-        <div className='mt-8 w-full flex flex-col flex-nowrap justify-around items-center'>
-          <Image
-            src={'https://spotiy-playlist-retriever-experimental.vercel.app/_next/static/media/user_img.6db01878.svg'}
-            width={160}
-            height={160}
-            alt='Defualt user image'
-          />
-          <p className='text-white font-normal text-xl mt-5 mb-2'>Sign In as</p>
-          <span className='bold-txt'>{session?.user?.name}</span>
-        </div>
-        <p className='opacity-70 mt-8 mb-5 underline cursor-pointer' onClick={() => signOut()}>Sign Out</p>
-      </div>
-    )
-  } else {
-    return (
-      <div className='max-w-[19rem] h-80 rounded-[2rem] border-4 border-solid border-white flex justify-around items-center flex-col flex-nowrap mt-10 ml-10'>
-        <Image
-          src={'https://spotiy-playlist-retriever-experimental.vercel.app/_next/static/media/sad_emoji.41405e6f.svg'}
-          width={160}
-          height={150}
-          alt='sad emoji'
-        />
-        <button onClick={() => signIn()} className='shadow-primary w-56 h-16 rounded-xl bg-white border-0 text-black text-3xl active:scale-[0.99]'>Sign In</button>
-      </div>
-    )
+const StyledLoginButton = styled.button`
+  display: inline-block;
+  background-color: var(--green);
+  color: var(--white);
+  border-radius: var(--border-radius-pill);
+  font-weight: 700;
+  font-size: var(--fz-lg);
+  padding: var(--spacing-sm) var(--spacing-xl);
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+    filter: brightness(1.1);
   }
+`
+
+const Login = () => {
+  const router = useRouter()
+  const { data: session } = useSession()
+  useEffect(() => {
+    console.log(session)
+    if (session && !session.error) {
+      router.push('/profile')
+    }
+  }, [session])
+
+  return (
+    <StyledLoginContainer>
+      <StyledLoginButton onClick={() => signIn('spotify')}>Log in to Spotify</StyledLoginButton>
+    </StyledLoginContainer>
+  )
 }
+
+
+export default Login;
