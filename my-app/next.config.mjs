f60b2b5f808cb4501fc530dad3c57ab96d570f68
John Kommala <johnwesleygithub@gmail.com>
2025-04-25 20:20:03 +0100

    allowing all images to render

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
   compiler: {
     styledComponents: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // Allow images from all domains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
