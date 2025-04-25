8e6035b45c26cf9a157ce796b0d0ce0c2c6c1a0d
John Kommala <johnwesleygithub@gmail.com>
2025-04-25 19:06:15 +0100

    added limit to recently played tracks

--- my-app/src/app/api/spotify/getRecentlyPlayed.js
+++ my-app/src/app/api/spotify/getRecentlyPlayed.js
@@ -5,6 +5,6 @@ import getWrapper from "~/app/api/spotify/getWrapper";
  * https://developer.spotify.com/documentation/web-api/reference/player/get-recently-played/
  */
 
-const getRecentlyPlayed = () => getWrapper('/me/player/recently-played')
+const getRecentlyPlayed = (limit = 50) => getWrapper(`/me/player/recently-played?limit=${limit}`) 
 
 export default getRecentlyPlayed
