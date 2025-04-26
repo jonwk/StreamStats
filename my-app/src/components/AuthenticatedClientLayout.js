5b4045292280a08af2bbaec992c66923c9666643
John Kommala <johnwesleygithub@gmail.com>
2025-04-27 00:43:49 +0100

    logout button in demo page

--- my-app/src/components/AuthenticatedClientLayout.js
+++ my-app/src/components/AuthenticatedClientLayout.js
@@ -1,16 +1,20 @@
 'use client'
 import { ThemeProvider } from 'styled-components'
-import {GlobalStyles, theme} from '~/styles'
+import { GlobalStyles, theme } from '~/styles'
 import { LogoutButton, ScrollToTop } from "~/components"
 import { useSession } from "next-auth/react"
+import { usePathname } from 'next/navigation'
 
 const AuthenticatedClientLayout = ({ children }) => {
   const { data: session } = useSession()
+  const pathname = usePathname()
+  const isDemo = pathname?.startsWith('/demo')
+
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        {session && <LogoutButton />}
+        {(session || isDemo) && <LogoutButton />}
         <ScrollToTop />
         {children}
       </ThemeProvider>
