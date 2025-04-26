15d827bae63da56e7095ad4fff01c161ad13a09b
John Kommala <johnwesleygithub@gmail.com>
2025-04-26 22:35:44 +0100

    demo and actual profile pages

---
+++ my-app/src/app/demo/profile/page.js
@@ -0,0 +1,61 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { Profile } from '~/components/Pages'
+
+const DemoProfilePage = () => {
+  const [data, setData] = useState({
+    profile: null,
+    playlists: null,
+    topArtists: null,
+    topTracks: null,
+    recentlyPlayed: null,
+  })
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchDemoData = async () => {
+      try {
+        const [
+          userProfile,
+          userPlaylists,
+          topArtistsData,
+          topTracksData,
+          recentlyPlayedData
+        ] = await Promise.all([
+          import('~/app/demo/data/userProfile.json'),
+          import('~/app/demo/data/userPlaylists.json'),
+          import('~/app/demo/data/topArtists.json'),
+          import('~/app/demo/data/topTracks.json'),
+          import('~/app/demo/data/userRecentlyPlayed.json')
+        ])
+
+        setData({
+          profile: userProfile.default,
+          playlists: userPlaylists.default,
+          topArtists: topArtistsData.default,
+          topTracks: topTracksData.default,
+          recentlyPlayed: recentlyPlayedData.default
+        })
+      } catch (error) {
+        console.error('Error loading demo data:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchDemoData()
+  }, [])
+
+  return (
+    <Profile
+      profile={data.profile}
+      playlists={data.playlists}
+      topArtists={data.topArtists}
+      topTracks={data.topTracks}
+      recentlyPlayed={data.recentlyPlayed}
+      loading={loading}
+    />
+  )
+}
+
+export default DemoProfilePage
--- my-app/src/app/profile/page.js
+++ my-app/src/app/profile/page.js
@@ -1,14 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
 import {
-  ArtistsGrid,
-  Loader,
-  PlaylistsGrid,
-  SectionWrapper,
-  TrackItem,
-  TrackList
-} from '~/components'
-import {
   getCurrentUserPlaylists,
   getCurrentUserProfile,
   getCurrentUserTopArtists,
@@ -16,17 +8,19 @@ import {
   getRecentlyPlayed
 } from '~/app/api/spotify'
 import FadeIn from '~/animations/FadeIn'
-import { StyledHeader, StyledTrackList } from '~/styles'
 import { catchErrors } from '~/util'
 import useLogoutOnError from '~/hooks/useLogoutOnError'
+import {Profile} from '~/components/Pages'
 
-const Profile = () => {
+const ProfilePage = () => {
   const handleLogoutError = useLogoutOnError()
-  const [profile, setProfile] = useState()
-  const [playlists, setPlaylists] = useState()
-  const [topArtists, setTopArtists] = useState()
-  const [topTracks, setTopTracks] = useState()
-  const [recentlyPlayed, setRecentlyPlayed] = useState()
+  const [data, setData] = useState({
+    profile: null,
+    playlists: null,
+    topArtists: null,
+    topTracks: null,
+    recentlyPlayed: null,
+  })
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -34,20 +28,28 @@ const Profile = () => {
       setLoading(true)
 
       try {
-        const userProfile = await getCurrentUserProfile()
-        setProfile(userProfile)
+        const [
+          userProfile,
+          userPlaylists,
+          topArtists,
+          recentlyPlayed,
+          topTracks
+        ] = await Promise.all([
+          getCurrentUserProfile(),
+          getCurrentUserPlaylists(),
+          getCurrentUserTopArtists(),
+          getRecentlyPlayed(),
+          getCurrentUserTopTracks()
+        ])
 
-        const userPlaylists = await getCurrentUserPlaylists()
-        setPlaylists(userPlaylists)
-
-        const topArtists = await getCurrentUserTopArtists()
-        setTopArtists(topArtists)
-
-        const userRecentlyPlayed = await getRecentlyPlayed()
-        setRecentlyPlayed(userRecentlyPlayed)
-
-        const topTracks = await getCurrentUserTopTracks()
-        setTopTracks(topTracks)
+        console.log('toptracks', topTracks)
+        setData({
+          profile: userProfile,
+          playlists: userPlaylists,
+          topArtists,
+          topTracks,
+          recentlyPlayed
+        })
       } catch (error) {
         console.log('Error fetching data:', error)
         await handleLogoutError()
@@ -61,68 +63,15 @@ const Profile = () => {
 
 
   return (
-    <div>
-      {loading ? (
-        <Loader />
-      ) : profile && (
-        <div>
-          <StyledHeader type="user">
-            <div className="header__inner">
-              {profile.images && profile.images.length > 0 && profile.images[0].url && (
-                <p>
-                  <img
-                    className="header__img"
-                    src={profile.images[0].url}
-                    alt="profile-pic"
-                  />
-                </p>
-              )}
-              <div>
-                <div className="header__overline">Profile</div>
-                <h1 className="header__name">{profile.display_name}</h1>
-                <p className="header__meta">
-                  {playlists && (
-                    <span>
-                      {playlists.total} Playlist
-                      {playlists.total === 1 ? '' : 's'}
-                    </span>
-                  )}
-                  <span>
-                    {profile.followers.total} Follower
-                    {profile.followers.total === 1 ? '' : 's'}
-                  </span>
-                </p>
-              </div>
-            </div>
-          </StyledHeader>
-
-          <main>
-            <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
-              {topArtists ? (<ArtistsGrid artists={topArtists.items.slice(0, 10)} />) : (<Loader />)}
-            </SectionWrapper>
-
-            <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
-              {topTracks ? (<TrackList tracks={topTracks.items.slice(0, 10)} />) : (<Loader />)}
-            </SectionWrapper>
-
-            <SectionWrapper title="Recently Played" seeAllLink="/recently-played">
-              {recentlyPlayed ? (
-                <StyledTrackList>
-                  {recentlyPlayed.items.slice(0, 10).map(({ track }, index) => (
-                    <TrackItem track={track} key={index} />
-                  ))}
-                </StyledTrackList>
-              ) : (<Loader />)}
-            </SectionWrapper>
-
-            <SectionWrapper title="Public Playlists" seeAllLink="/playlists">
-              {playlists ? (<PlaylistsGrid playlists={playlists.items.slice(0, 10)} />) : (<Loader />)}
-            </SectionWrapper>
-          </main>
-        </div>
-      )}
-    </div >
+    <Profile
+      profile={data.profile}
+      playlists={data.playlists}
+      topArtists={data.topArtists}
+      topTracks={data.topTracks}
+      recentlyPlayed={data.recentlyPlayed}
+      loading={loading}
+    />
   )
 }
 
-export default Profile
+export default ProfilePage
