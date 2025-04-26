5d83ca161e83eb9036915452fbdac66d229eccc6
John Kommala <johnwesleygithub@gmail.com>
2025-04-26 18:52:02 +0100

    layout -> serverside, to fit in metadata

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,22 +1,34 @@
-'use client';
-
 import AuthProvider from "~/app/AuthProvider";
 import StyledComponentsRegistry from '~/lib/registry'
-import theme from '~/styles/theme'
-import GlobalStyles from '~/styles/GlobalStyles'
-import { ThemeProvider } from 'styled-components'
-import { useSession } from "next-auth/react"
-import { LogoutButton, ScrollToTop } from "~/components"
+import { AuthenticatedClientLayout } from '~/components'
 
-const AuthenticatedLayout = ({ children }) => {
-  const { data: session } = useSession()
-  return (
-    <>
-      {session && <LogoutButton />}
-      <ScrollToTop />
-      {children}
-    </>
-  )
+export const metadata = {
+  title: 'StreamStats',
+  description: 'Web app for visualzing personalized Spotify Stats',
+  themeColor: '#1DB954',
+  icons: {
+    icon: [
+      { url: '/favicons/favicon.ico', sizes: 'any' },
+      { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicons/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/favicons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ]
+  },
+  manifest: '/favicons/site.webmanifest',
+  openGraph: {
+    title: 'StreamStats',
+    description: 'Web app for visualzing personalized Spotify Stats',
+    images: '/og.png'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'StreamStats',
+    description: 'Web app for visualzing personalized Spotify Stats',
+    creator: '@jonwk',
+    images: '/og.png'
+  }
 }
 
 export default function RootLayout({ children }) {
@@ -25,12 +37,9 @@ export default function RootLayout({ children }) {
       <body className='streamstas-body'>
         <AuthProvider>
           <StyledComponentsRegistry>
-            <GlobalStyles />
-            <ThemeProvider theme={theme}>
-              <AuthenticatedLayout>
-                {children}
-              </AuthenticatedLayout>
-            </ThemeProvider>
+            <AuthenticatedClientLayout>
+              {children}
+            </AuthenticatedClientLayout>
           </StyledComponentsRegistry>
         </AuthProvider>
       </body>
