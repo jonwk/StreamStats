dcc25e668300ee435992359c6f96ac49e645f027
John Kommala <johnwesleygithub@gmail.com>
2025-04-26 22:35:29 +0100

    created new profile page component

---
+++ my-app/src/components/Pages/Profile.js
@@ -0,0 +1,78 @@
+'use client'
+import {
+  ArtistsGrid,
+  Loader,
+  PlaylistsGrid,
+  SectionWrapper,
+  TrackItem,
+  TrackList
+} from '~/components'
+import { StyledHeader, StyledTrackList } from '~/styles'
+
+const ProfileContent = ({ profile, playlists, topArtists, topTracks, recentlyPlayed, loading }) => {
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : profile && (
+        <div>
+          <StyledHeader type="user">
+            <div className="header__inner">
+              {profile.images && profile.images.length > 0 && profile.images[0].url && (
+                <p>
+                  <img
+                    className="header__img"
+                    src={profile.images[0].url}
+                    alt="profile-pic"
+                  />
+                </p>
+              )}
+              <div>
+                <div className="header__overline">Profile</div>
+                <h1 className="header__name">{profile.display_name}</h1>
+                <p className="header__meta">
+                  {playlists && (
+                    <span>
+                      {playlists.total} Playlist
+                      {playlists.total === 1 ? '' : 's'}
+                    </span>
+                  )}
+                  <span>
+                    {profile.followers.total} Follower
+                    {profile.followers.total === 1 ? '' : 's'}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+
+          <main>
+            <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
+              {topArtists ? (<ArtistsGrid artists={topArtists.items.slice(0, 10)} />) : (<Loader />)}
+            </SectionWrapper>
+
+            <SectionWrapper title="Top tracks this month" seeAllLink="/top-tracks">
+              {topTracks ? (<TrackList tracks={topTracks.items.slice(0, 10)} />) : (<Loader />)}
+            </SectionWrapper>
+
+            <SectionWrapper title="Recently Played" seeAllLink="/recently-played">
+              {recentlyPlayed ? (
+                <StyledTrackList>
+                  {recentlyPlayed.items.slice(0, 10).map(({ track }, index) => (
+                    <TrackItem track={track} key={index} />
+                  ))}
+                </StyledTrackList>
+              ) : (<Loader />)}
+            </SectionWrapper>
+
+            <SectionWrapper title="Public Playlists" seeAllLink="/playlists">
+              {playlists ? (<PlaylistsGrid playlists={playlists.items.slice(0, 10)} />) : (<Loader />)}
+            </SectionWrapper>
+          </main>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ProfileContent
