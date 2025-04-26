53981d5082c8c64565ed20b6a79cab835e7be5f5
John Kommala <johnwesleygithub@gmail.com>
2025-04-26 16:09:08 +0100

    useLogoutOnError hook

---
+++ my-app/src/hooks/useLogoutOnError.js
@@ -0,0 +1,22 @@
+import { getSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { signOut } from 'next-auth/react'
+
+/**
+ * Custom hook for handling authentication errors
+ * @returns {Function} Function to handle authentication errors
+ */
+const useLogoutOnError = () => { 
+  const router = useRouter()
+
+  return async () => {
+    const session = await getSession()
+    console.log('got here', session)
+    if (!session) {
+      signOut({callbackUrl: '/'})
+      router.push('/')
+    }
+  }
+}
+
+export default useLogoutOnError
