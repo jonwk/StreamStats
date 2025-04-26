ce699d8342e0ca645f395b74a15ba748ec70e29d
John Kommala <johnwesleygithub@gmail.com>
2025-04-26 22:50:45 +0100

    created topArtists page

---
+++ my-app/src/components/Pages/TopArtists.js
@@ -0,0 +1,24 @@
+'use client'
+import { ArtistsGrid, Loader, SectionWrapper, TimeRangeButtons } from '~/components'
+
+const TopArtists = ({ topArtists, activeRange, setActiveRange, isLoading, isDemo = false }) => {
+  return (
+    <main>
+      <SectionWrapper title="Top Artists" breadcrumb={true}>
+        <TimeRangeButtons
+          activeRange={activeRange}
+          setActiveRange={setActiveRange}
+        />
+        <div style={{ minHeight: '60vh', position: 'relative' }}>
+          {!isLoading && topArtists && topArtists.items ? (
+            <ArtistsGrid artists={topArtists.items} />
+          ) : (
+            <Loader />
+          )}
+        </div>
+      </SectionWrapper>
+    </main>
+  )
+}
+
+export default TopArtists
--- my-app/src/components/Pages/index.js
+++ my-app/src/components/Pages/index.js
@@ -1,1 +1,2 @@
 export { default as Profile } from '~/components/Pages/Profile'
+export { default as TopArtists } from '~/components/Pages/TopArtists'
