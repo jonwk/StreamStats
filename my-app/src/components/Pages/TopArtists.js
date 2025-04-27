dbf342ebd34085c712716448c7ca0a9b958abcc5
John Kommala <johnwesleygithub@gmail.com>
2025-04-27 01:13:10 +0100

    MotionFade to TopArtists Page

--- my-app/src/components/Pages/TopArtists.js
+++ my-app/src/components/Pages/TopArtists.js
@@ -1,23 +1,28 @@
 'use client'
 import { ArtistsGrid, Loader, SectionWrapper, TimeRangeButtons } from '~/components'
+import { MotionFade } from '~/animations'
 
 const TopArtists = ({ topArtists, activeRange, setActiveRange, isLoading, isDemo = false }) => {
   return (
-    <main>
-      <SectionWrapper title="Top Artists" breadcrumb={true} isDemo={isDemo}>
-        <TimeRangeButtons
-          activeRange={activeRange}
-          setActiveRange={setActiveRange}
-        />
-        <div style={{ minHeight: '60vh', position: 'relative' }}>
-          {!isLoading && topArtists && topArtists.items ? (
-            <ArtistsGrid artists={topArtists.items} />
-          ) : (
-            <Loader />
-          )}
-        </div>
-      </SectionWrapper>
-    </main>
+    <MotionFade>
+      <main>
+        <SectionWrapper title="Top Artists" breadcrumb={true} isDemo={isDemo}>
+          <TimeRangeButtons
+            activeRange={activeRange}
+            setActiveRange={setActiveRange}
+          />
+          <div style={{ minHeight: '60vh', position: 'relative' }}>
+            {!isLoading && topArtists && topArtists.items ? (
+              <MotionFade key={activeRange} delay={0.2}>
+                <ArtistsGrid artists={topArtists.items} />
+              </MotionFade>
+            ) : (
+              <Loader />
+            )}
+          </div>
+        </SectionWrapper>
+      </main>
+    </MotionFade>
   )
 }
 
