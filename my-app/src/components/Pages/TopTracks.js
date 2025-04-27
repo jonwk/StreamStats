35755deec49ff48111eedc54e5d704908566fa8c
John Kommala <johnwesleygithub@gmail.com>
2025-04-27 01:14:32 +0100

    motion fade for top tracks

--- my-app/src/components/Pages/TopTracks.js
+++ my-app/src/components/Pages/TopTracks.js
@@ -1,23 +1,28 @@
 'use client'
 import { Loader, SectionWrapper, TimeRangeButtons, TrackList } from '~/components'
+import { MotionFade } from '~/animations'
 
 const TopTracks = ({ topTracks, activeRange, setActiveRange, isLoading, isDemo = false }) => {
   return (
-    <main>
-      <SectionWrapper title="Top Tracks" breadcrumb={true} isDemo={isDemo}>
-        <TimeRangeButtons
-          activeRange={activeRange}
-          setActiveRange={setActiveRange}
-        />
-        <div style={{ minHeight: '60vh', position: 'relative' }}>
-          {!isLoading && topTracks && topTracks.items ? (
-            <TrackList tracks={topTracks.items} />
-          ) : (
-            <Loader />
-          )}
-        </div>
-      </SectionWrapper>
-    </main>
+    <MotionFade>
+      <main>
+        <SectionWrapper title="Top Tracks" breadcrumb={true} isDemo={isDemo}>
+          <TimeRangeButtons
+            activeRange={activeRange}
+            setActiveRange={setActiveRange}
+          />
+          <div style={{ minHeight: '60vh', position: 'relative' }}>
+            {!isLoading && topTracks && topTracks.items ? (
+              <MotionFade key={activeRange} delay={0.2}>
+                <TrackList tracks={topTracks.items} />
+              </MotionFade>
+            ) : (
+              <Loader />
+            )}
+          </div>
+        </SectionWrapper>
+      </main>
+    </MotionFade>
   )
 }
 
