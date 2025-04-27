9635ca07e95fa4061dd03b2311c1203caa8b4f16
John Kommala <johnwesleygithub@gmail.com>
2025-04-27 02:06:37 +0100

    eslint config

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -2,75 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import pluginReact from 'eslint-plugin-react'
 import { defineConfig } from 'eslint/config'
-
-
-// const eslintConfig = [
-//   js.configs.recommended,
-//   ...compat.extends(['next']),
-//   {
-//     files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-//     languageOptions: {
-//       ecmaVersion: 'latest',
-//       sourceType: 'module',
-//       globals: {
-//         ...globals.browser,
-//         ...globals.node,
-//         ...globals.es2021
-//       },
-//       parserOptions: {
-//         ecmaFeatures: {
-//           jsx: true
-//         }
-//       }
-//     },
-//     plugins: {
-//       ...compat.plugins(['react', 'unicorn'])
-//     },
-//     rules: {
-// 'semi': ['error', 'never'],
-// 'quotes': ['error', 'single', {
-//   'avoidEscape': true
-// }],
-//       'no-multi-spaces': 'error',
-//       'comma-spacing': ['error', {
-//         'before': false,
-//         'after': true
-//       }],
-//       'object-curly-spacing': ['error', 'always'],
-//       'padding-line-between-statements': ['error', {
-//         'blankLine': 'never',
-//         'prev': 'import',
-//         'next': 'import'
-//       }],
-//       'unicorn/filename-case': ['error', {
-//         'cases': {
-//           'camelCase': true,
-//           'pascalCase': true
-//         }
-//       }],
-//       'unicorn/prevent-abbreviations': ['error', {
-//         'allowList': {
-//           'args': true,
-//           'props': true,
-//           'Props': true,
-//           'prop': true,
-//           'ref': true,
-//           'Ref': true,
-//           'temp': true
-//         }
-//       }],
-//       'unicorn/no-array-reduce': 'off',
-//       'unicorn/no-array-for-each': 'off',
-//       'react/prop-types': 'off',
-//       'react/function-component-definition': ['error', {
-//         'namedComponents': 'arrow-function',
-//         'unnamedComponents': 'arrow-function'
-//       }]
-//     }
-//   }
-// ]
-
-// export default eslintConfig
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
 const rules = {
   'semi': ['error', 'never'],
@@ -117,12 +49,25 @@ const rules = {
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: { js }, extends: ['js/recommended'],
-    rules: rules
+    plugins: { js, unicorn: eslintPluginUnicorn },
+    extends: ['js/recommended'],
+    rules: rules,
+    settings: {
+      'react': {
+        'version': 'detect'
+      }
+    }
   },
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
-    languageOptions: { globals: globals.browser }
+    plugins: { js, unicorn: eslintPluginUnicorn },
+    languageOptions: { globals: globals.browser },
+    rules: rules,
+    settings: {
+      'react': {
+        'version': 'detect'
+      }
+    }
   },
   pluginReact.configs.flat.recommended,
 ])
