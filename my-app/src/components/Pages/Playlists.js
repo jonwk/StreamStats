0cd4a8decb75efa7b00daafba6306d0d482512a4
John Kommala <johnwesleygithub@gmail.com>
2025-04-27 01:18:36 +0100

    motion fade to playlists

--- my-app/src/components/Pages/Playlists.js
+++ my-app/src/components/Pages/Playlists.js
@@ -1,19 +1,24 @@
 'use client'
 import { Loader, PlaylistsGrid, SectionWrapper } from '~/components'
+import { MotionFade } from '~/animations'
 
 const Playlists = ({ playlists, loading, isDemo = false }) => {
   return (
-    <main>
-      <SectionWrapper title="Public Playlists" breadcrumb={true} isDemo={isDemo}>
-        <div style={{ minHeight: '60vh', position: 'relative' }}>
-          {!loading && playlists ? (
-            <PlaylistsGrid playlists={playlists} isDemo={isDemo} />
-          ) : (
-            <Loader />
-          )}
-        </div>
-      </SectionWrapper>
-    </main>
+    <MotionFade>
+      <main>
+        <SectionWrapper title="Public Playlists" breadcrumb={true} isDemo={isDemo}>
+          <div style={{ minHeight: '60vh', position: 'relative' }}>
+            {!loading && playlists ? (
+              <MotionFade delay={0.2}>
+                <PlaylistsGrid playlists={playlists} isDemo={isDemo} />
+              </MotionFade>
+            ) : (
+              <Loader />
+            )}
+          </div>
+        </SectionWrapper>
+      </main>
+    </MotionFade>
   )
 }
 
