86375c7b99cf42db9cb8c3bc9fdf157d34351f63
John Kommala <johnwesleygithub@gmail.com>
2025-04-27 01:17:47 +0100

    motion fade to recently played

--- my-app/src/components/Pages/RecentlyPlayed.js
+++ my-app/src/components/Pages/RecentlyPlayed.js
@@ -1,19 +1,24 @@
 'use client'
 import { Loader, SectionWrapper, TrackList } from '~/components'
+import { MotionFade } from '~/animations'
 
 const RecentlyPlayed = ({ recentlyPlayed, isLoading, isDemo = false }) => {
   return (
-    <main>
-      <SectionWrapper title="Recently Played" breadcrumb={true} isDemo={isDemo}>
-        <div style={{ minHeight: '60vh', position: 'relative' }}>
-          {!isLoading && recentlyPlayed && recentlyPlayed.items ? (
-            <TrackList tracks={recentlyPlayed.items.map(({ track }) => track)} />
-          ) : (
-            <Loader />
-          )}
-        </div>
-      </SectionWrapper>
-    </main>
+    <MotionFade>
+      <main>
+        <SectionWrapper title="Recently Played" breadcrumb={true} isDemo={isDemo}>
+          <div style={{ minHeight: '60vh', position: 'relative' }}>
+            {!isLoading && recentlyPlayed && recentlyPlayed.items ? (
+              <MotionFade delay={0.2}>
+                <TrackList tracks={recentlyPlayed.items.map(({ track }) => track)} />
+              </MotionFade>
+            ) : (
+              <Loader />
+            )}
+          </div>
+        </SectionWrapper>
+      </main>
+    </MotionFade>
   )
 }
 
